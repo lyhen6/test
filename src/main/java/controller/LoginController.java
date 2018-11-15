d0f8548921490660e64bc7f6c7488dca361aaa7a
liuyuan <liuyuan@317hu.com>
2018-11-15 11:27:07 +0800

    test

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,9 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
